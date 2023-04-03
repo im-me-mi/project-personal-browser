d792c2aada76a1692cb50054dc70ae4e434a313c
Miryeong Lee <lmryeong23@gmail.com>
2023-04-04 02:55:31 +0900

    first commit

---
+++ js/mainTitle.js
@@ -0,0 +1,33 @@
+const loginForm = document.querySelector("form#login-form");
+const loginInput = document.querySelector("form#login-form input");
+const mainTitle = document.querySelector("h1#main-title");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLogSubmit(event) {
+  event.preventDefault();
+  const userName = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, userName);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  loadMainTitle(userName);
+}
+
+function loadMainTitle(userName) {
+  mainTitle.innerText = `Hello ${userName}`;
+  mainTitle.classList.remove(HIDDEN_CLASSNAME);
+}
+
+//localstorage에 저장하기까지의 실행과정
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLogSubmit);
+  //localstorage에 유저이름이 저장되어 있지 않을시 실행되는 동작
+  //(form을 다시 불러와 저장하기까지의 실행과정을 거친다.)
+} else {
+  loadMainTitle(savedUserName);
+  //localstorage에 유저이름이 저장되어 있을시 실행되는 동작
+}
