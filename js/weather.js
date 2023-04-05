1351e385143226f5bbb280684ae5b22b614e3c29
Miryeong Lee <lmryeong23@gmail.com>
2023-04-06 02:57:19 +0900

    날씨 및 위치설정 기능 생성

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+const API_KEYS = "e82f9e933332547f57e6ff98ba9efc95";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log("위치를 알아냈음", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEYS}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const geo = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      const temp = data.main.temp;
+      geo.innerText = data.name;
+      weather.innerText = `/ ${temp}도 ${data.weather[0].main}`;
+    });
+}
+
+function onGoError() {
+  alert("위치를 설정할 수 없습니다.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGoError);
