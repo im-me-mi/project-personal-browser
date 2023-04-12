bf14f04aac5a5693ed12b24822ba6c93738bb2b1
Miryeong Lee <lmryeong23@gmail.com>
2023-04-13 03:33:30 +0900

    index.html tag수정으로인한 querySelector 할당 tag 수정

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,11 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("p#clock");
 
 function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  clock.innerText = `현재시각은 ${hours}시 ${minutes}분 입니다.`;
 }
 
 getClock();
